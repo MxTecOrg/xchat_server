9f7947f2d39a87c27bb62a8a0152b77313fe65c0
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-15 18:03:52 -0400

    Startup of Server File

--- server.js
+++ server.js
@@ -1,1 +1,47 @@
-/**/
+
+/*****************
+ * Server Config *
+ *****************/
+ 
+const config = require("./config.js");
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const router = require(config.LOGIC + "/router.js");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const DB = require(config.LOGIC + "/helper/DB.js");
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+/* Express router */
+app.use("/", router);
+// Wakeup route
+app.post("/wakeup", (req, res) => {
+    res.json({
+        status: true
+    });
+});
+//Error 404
+app.use((req , res) => { 
+    res.json({
+    status: false, message: "ERROR 404"});
+});
+
+/* Loading all db data */
+DB.loadAll();
+
+server.listen(config.PORT , (log) => console.log("Server running on port:" + config.PORT));
+
+/* Exporting and Importing socket.io methods */
+module.exports = io;
+
+require(config.LOGIC + "/socket.js");
+
+/* Start saving db periodically */
+DB.autoSave(60 * 1000);
