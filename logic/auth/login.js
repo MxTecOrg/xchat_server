9fcf46e1afdf070a221a1281b6411db8aeaf2f68
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-15 18:46:55 -0400

    Login File

---
+++ logic/auth/login.js
@@ -0,0 +1,57 @@
+const config = require("../../config.js");
+const DB = require(config.LOGIC + "/helpers/DB.js");
+const authenticator = require("./authenticator.js");
+const bcrypt = require("bcryptjs");
+
+/* function login
+* @Method : POST
+* @param req {body : {username , password , app_token}}
+* @param res {}
+*/
+
+function login (req, res) {
+    if (!req.body) return res.json({status : false , data : "NO_DATA"});
+    let username,
+    password;
+    try {
+      const body = req.body;
+        username = body.username;
+        password = body.password;
+    } catch (err) {
+        return res.json({
+            status: false,
+            data: "DATA_ERROR",
+            error: err
+        });
+    }
+
+    const account = DB.findUserByName(username);
+
+    if (!account) {
+        return res.json({
+            status: false,
+            data: "WRONG_USER"
+        });
+    }
+
+
+    if (!bcrypt.compareSync(password, account.password)) {
+        return res.json({
+            status: false,
+            data: "WRONG_USER"
+        });
+    }
+
+    if (account.id == null || account.id == undefined) {
+        return res.json({
+            status: false,
+            data: "ACC_ERROR"
+        });
+    }
+    return res.json({
+        status: true,
+        data: authenticator.generate(account.id)
+    });
+}
+
+module.exports = login;
