227e19d3901751a703a8e6122e3037cec8211241
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-15 18:10:48 -0400

    Startup of Config File

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
-/**/
+/* Base Config */
+const config = { 
+    URL: "https://xchat_server.glitch.me", 
+    PORT: process.env.PORT || 8081, //port
+    DIRNAME: __dirname, //root folder
+    DB: __dirname + "/database", //database
+    LOGIC: __dirname + "/logic", //logic 
+    TOKEN: { 
+        secret: "super_secret_token_keyword", 
+        expire: "6h" 
+    }, 
+    SERVER: { version: "v0.0.1" }
+ }; 
+ 
+ module.exports = config;
