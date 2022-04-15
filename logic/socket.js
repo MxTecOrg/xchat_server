4e4824e1055031f23691ee1c1bc22a82b61f93f7
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-15 18:21:57 -0400

    Startup of main Socket file

---
+++ logic/socket.js
@@ -0,0 +1,27 @@
+const config = require("../config.js");
+const io = require(config.DIRNAME + "/server.js");
+const auth = require(config.LOGIC + "/auth/authenticator.js");
+const DB = require(config.LOGIC + "/helpers/DB.js");
+const load = require(config.LOGIC + "/socket/Socks.js");
+
+io.on("connection" , (socket) => {
+    const token = socket.handshake.query.token;
+    if(!token) return socket.disconnect();
+    
+    const id = auth.verify(token);
+    if(!id) return socket.disconnect();
+    
+    if(!DB.findUserById(id)) return socket.disconnect();
+    
+    io.sockets[id] = socket;
+    DB.setUserValue(id , "isOnline" , true);
+    
+    Socks(io , socket , id);
+    
+    socket.on("disconnect" , (data) => {
+        DB.setUserValue(id , "isOnline" , false);
+        delete io.socket[id];
+    });
+});
+
+module.exports = true;
