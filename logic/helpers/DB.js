26dfe8e36f25de8c30811bb089d289a7629c06c3
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-16 16:41:08 -0400

    update DB.js

--- logic/helpers/DB.js
+++ logic/helpers/DB.js
@@ -4,8 +4,9 @@
 
 const fs = require("fs");
 const config = require("../../config.js");
+const uid = require(config.LOGIC + "/helpers/uid.js");
 
-var USERS = {} , BOTS = {} , CHANNELS = {} , TOKEN_PAIRS = {};
+var USERS = {}, ROOMS = {}, BOTS = {}, CHANNELS = {}, TOKEN_PAIRS = {};
 
 const DB = {
     loadUsers: function () {
@@ -20,9 +21,23 @@ const DB = {
         }
         console.timeEnd("Users loaded in:");
     },
-    
+
+    loadRooms: function () {
+        console.time("Rooms loaded in:")
+        const _rooms = fs.readdirSync(config.DB + "/rooms/");
+        try {
+            for (let u of _rooms) {
+                ROOMS[u.split(".")[0]] = JSON.parse(fs.readFileSync(config.DB + "/rooms/" + u));
+            }
+        }catch(err) {
+            console.error(err);
+        }
+        console.timeEnd("Rooms loaded in:");
+    },
+
     loadAll: function() {
         this.loadUsers();
+        this.loadRooms();
     },
 
     saveUsers: async function() {
@@ -33,14 +48,25 @@ const DB = {
         console.timeEnd("Users saved in:");
         return true;
     },
-    
-    
+
+    saveRooms: async function() {
+        console.time("Rooms saved in:");
+        for (let u in ROOMS) {
+            await fs.writeFile(config.DB + "/rooms/" + u + ".json", JSON.stringify(ROOMS[u]), ()=> {});
+        }
+        console.timeEnd("Rooms saved in:");
+        return true;
+    },
+
+
     autoSave: function(time) {
         const su = this.saveUsers;
+        const sr = this.saveRooms;
         async function s() {
             await su();
+            await sr();
         }
-        setInterval(s , time);
+        setInterval(s, time);
     },
 
     addUser: function(id , data) {
@@ -92,29 +118,29 @@ const DB = {
     findAllUsers: function(key , condition , value) {
         let f = [];
         for (let u in USERS) {
-            switch(condition){
+            switch (condition) {
                 case "==":
-                    if(USERS[u][key] == value) f.push(USERS[u]);
+                    if (USERS[u][key] == value) f.push(USERS[u]);
                     return f;
                     break;
                 case "!=":
-                    if(USERS[u][key] != value) f.push(USERS[u]);
+                    if (USERS[u][key] != value) f.push(USERS[u]);
                     return f;
                     break;
                 case "<":
-                    if(USERS[u][key] < value) f.push(USERS[u]);
+                    if (USERS[u][key] < value) f.push(USERS[u]);
                     return f;
                     break;
                 case ">":
-                    if(USERS[u][key] > value) f.push(USERS[u]);
+                    if (USERS[u][key] > value) f.push(USERS[u]);
                     return f;
                     break;
                 case "<=":
-                    if(USERS[u][key] <= value) f.push(USERS[u]);
+                    if (USERS[u][key] <= value) f.push(USERS[u]);
                     return f;
                     break;
                 case ">=":
-                    if(USERS[u][key] >= value) f.push(USERS[u]);
+                    if (USERS[u][key] >= value) f.push(USERS[u]);
                     return f;
                     break;
                 default:
@@ -125,11 +151,11 @@ const DB = {
         }
         return f;
     },
-    
+
     getUserValue: function(id , key) {
-        if(USERS[id]){
+        if (USERS[id]) {
             return USERS[id][key];
-        }else return null;
+        } else return null;
     },
 
     setUserValue: function(id , key , value) {
@@ -145,18 +171,143 @@ const DB = {
             return true;
         } else return null;
     },
-    
-    addTokenPair : function (token , _token){
+
+    addTokenPair: function (token , _token) {
         TOKEN_PAIRS[token] = _token;
     },
-    
-    getTokenPair: function (token){
-        return TOKEN_PAIRS[token]
+
+    getTokenPair: function (token) {
+        return TOKEN_PAIRS[token];
+    },
+
+    delTokenPair: function (token) {
+        delete TOKEN_PAIRS[token];
+    },
+
+    createP2PRoom: function (user , user2) {
+        if (!USERS[user2] || USERS[user2].banList.includes(user)) return false;
+        let Room = DB.getRoom(user + "-" + user2) || DB.getRoom(user2 + "-" + user);
+        if (Room) return false;
+        Room = user + "-" + user2;
+        ROOMS[Room] = {
+            id: Room,
+            type: "private",
+            secure: uid.alphanum(32),
+            pinned: [],
+            bgColor: "SYSTEM",
+            textColor: "SYSTEM",
+            messages: [{
+                mess_id: uid.num(6),
+                user_id: "SYSTEM",
+                user_nick: "SYSTEM",
+                user_color: "SYSTEM",
+                chat_id: Room,
+                type: "text",
+                key: "",
+                reply: "",
+                isEdited: false,
+                receivedBy: [],
+                seenBy: [],
+                message: "Se ha iniciado el chat privado seguro.",
+                date: new Date()
+            }]
+        };
+        return ROOMS[Room]
+    },
+
+    createRoom: function (id , owner , name , desc , pic , members , type) {
+        let mem = [owner];
+        for (let m of members) {
+            if (USERS[m] && USERS[m].acceptInvitations) {
+                mem.push(m);
+            }
+        }
+
+        ROOMS[id] = {
+            id: id,
+            pic: (!pic ? "": pic),
+            type: "group",
+            gType: (type ? type: "public"),
+            name: (name ? name: "group-" + uid.alphanum(5)),
+            desc: (desc ? desc: "El admin es muy vago como para poner una descripción"),
+            bgColor: "SYSTEM",
+            textColor: "SYSTEM",
+            owner: owner,
+            admins: [],
+            members: mem,
+            banList : [],
+            bots: [],
+            pinned: [],
+            messages: [{
+                mess_id: uid.num(6),
+                user_id: "SYSTEM",
+                user_nick: "SYSTEM",
+                user_color: "SYSTEM",
+                chat_id: id,
+                type: "text",
+                key: "",
+                reply: "",
+                isEdited: false,
+                receivedBy: [],
+                seenBy: [],
+                message: "Se ha creado la sala \"" + this.name + "\"",
+                inline: [],
+                keyboard: [],
+                date: new Date()
+            }]
+        };
+
+        return ROOMS[id].messages;
+    },
+
+    getRoom: function(id) {
+        if (ROOMS[id]) return ROOMS[id];
+        else return null;
+    },
+
+    getRoomMembs: function(id) {
+        const r = this.getRoom(id);
+        if (r && r.members) return r.members;
+        else return null;
+    },
+
+    getRoomMess: function (id) {
+        const r = this.getRoom(id);
+        if (r && r.messages) return r.messages;
+        else return null;
+    },
+
+    getRoomMessFrom: function (id , mess_id) {
+        const r = this.getRoom(id);
+        let mess = [];
+        let found = false;
+        if (r && r.messages) {
+            for (let m of r.messages) {
+                if(m.mess_id == mess_id) found = true;
+                if (found) {
+                    mess.push(m)
+                }
+            }
+            if(found) return mess;
+            else return null;
+        } else return null;
     },
     
-    delTokenPair: function (token){
-        delete TOKEN_PAIRS[token]
+    getNewMess : function (id){
+        const user = this.findUserById(id);
+        
+        if(user) {
+            const rooms = user.rooms;
+            let allMess = [];
+            for(let r of rooms){
+                const mess = this.getRoomMessFrom(r.id , r.lastMess);
+                if(mess) allMess.push(mess);
+            }
+            return allMess;
+        }else return null;
     }
+
+
 };
 
 module.exports = DB;
