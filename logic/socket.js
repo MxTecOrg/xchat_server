cb395fe49f1295ccc02359fcca5892f33bedbc5f
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-16 10:30:50 -0400

    update socket.js

--- logic/socket.js
+++ logic/socket.js
@@ -4,24 +4,35 @@ const auth = require(config.LOGIC + "/auth/authenticator.js");
 const DB = require(config.LOGIC + "/helpers/DB.js");
 //const load = require(config.LOGIC + "/socket/Socks.js");
 
-io.on("connection" , (socket) => {
-    const token = socket.handshake.query.token;
-    if(!token) return socket.disconnect();
-    
-    const id = auth.verify(token);
-    if(!id) return socket.disconnect();
-    
-    if(!DB.findUserById(id)) return socket.disconnect();
-    
-    io.sockets[id] = socket;
-    DB.setUserValue(id , "isOnline" , true);
-    
-    //Socks(io , socket , id);
-    
-    socket.on("disconnect" , (data) => {
-        DB.setUserValue(id , "isOnline" , false);
-        delete io.socket[id];
-    });
+io.on("connection", (socket) => {
+    const token = socket.handshake.query.token,
+    api = socket.handshake.query.api,
+    bot = socket.handshake.query.bot;
+
+    const typo = (api ? "api": (bot ? "bot": (token ? "token": "none")));
+
+    switch (typo) {
+        case "token":
+            const id = auth.verify(token);
+            if (!id) return socket.disconnect();
+
+            if (!DB.findUserById(id)) return socket.disconnect();
+
+            io.sockets[id] = socket;
+            DB.setUserValue(id, "isOnline", true);
+
+            //Socks(io , socket , id);
+
+            socket.on("disconnect", (data) => {
+                DB.setUserValue(id, "isOnline", false);
+                delete io.socket[id];
+            });
+            break;
+        case "api":
+            break;
+        case "bot":
+            break;
+    }
 });
 
 module.exports = true;
