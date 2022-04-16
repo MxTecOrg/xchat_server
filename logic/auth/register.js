16a973ee8771bb31f7769400a2595af8827a3c34
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-16 01:01:17 -0400

    update register.js

--- logic/auth/register.js
+++ logic/auth/register.js
@@ -105,30 +105,6 @@ const register = async (req , res) => {
   
     try {
         DB.addUser(account.id , account);
-        
-        const nodemailer = require("nodemailer");
-        const transport = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: "fireshoot.dev@gmail.com",
-                pass: "hnm4Pz9h"
-            }
-        });
-
-        const message = {
-            from: "Infinity-Dungeon Team.",
-            to: email,
-            subject: "Verificación de Cuenta.",
-            text:
-            "Para verificar su cuenta siga el siguiente link:\n" +
-            config.URL +
-            "/auth/verify/" +
-            account.id
-        };
-
-        transport.sendMail(message, function(err, info) {
-            if (err) {} else {}
-        });
 
         return res.json({
             status: true,
