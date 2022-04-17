94b7a16c2ba1835e01146e093030a0544e7932ba
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-16 22:55:18 -0400

    update config.js

--- config.js
+++ config.js
@@ -9,7 +9,15 @@ const config = {
         secret: "super_secret_token_keyword", 
         expire: "6h" 
     }, 
-    SERVER: { version: "v0.0.1" }
+    SERVER: { version: "v0.0.1" },
+    ROOMS : {
+        chats_mem : 100,
+        channels_mem : 250,
+        bots_mem : 500,
+        chat_mess_tol : 250,
+        channels_mess_tol : 500,
+        bots_mess_tol : 100
+    }
  }; 
  
  module.exports = config;
