69f19bfee53ae4a0b05df5ad58ddf0105c87eef3
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-17 19:59:33 -0400

    update client.js

--- logic/client/client.js
+++ logic/client/client.js
@@ -3,7 +3,7 @@ const config = require("../../config.js");
 const chat = require("./chat");
 
 const client = async (io , socket , id) => {
-    chat(io , socket , id);
+    await chat(io , socket , id);
 };
 
 module.exports = client;
