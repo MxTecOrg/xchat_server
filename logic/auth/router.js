d1d78fe8799c727be6dde31e09751236c72f6553
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-18 12:51:15 -0400

    update router.js

--- logic/auth/router.js
+++ logic/auth/router.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const login = require("./login.js"); 
 const register = require("./register.js");
 const verify = require("./verify.js");
+const resendToken = require("./resendToken.js");
 
 router.post("/login" , (req , res) => login(req, res));
 router.post("/register" , (req , res) => register(req , res));
 router.post("/verify" , (req , res) => verify(req , res));
+router.post("/resendToken" , (req , res) => resendToken(req , res));
 
 module.exports = router;
