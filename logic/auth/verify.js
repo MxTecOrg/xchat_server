8ef978d4bb9df30fbdef6b9e5019f3277ace50ae
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-18 12:21:34 -0400

    Update verify.js

--- logic/auth/verify.js
+++ logic/auth/verify.js
@@ -1,1 +1,28 @@
 /**/
+const config = require("../../config.js");
+const DB = require(config.LOGIC + "/helpers/DB.js"):
+
+const verify = async (req , res) => {
+    if(!req.body.email || !req.body.token){
+        return res.json({
+            status : false,
+            data : "WRONG_DATA"
+        });
+    }
+  
+    const pair = DB.getPairToken(req.body.email);
+    if(pair != req.body.token){
+        return res.json({
+            status : false,
+            data : "WRONG_TOKEN"
+        });
+    }
+  
+    const user = await DB.findUserByMail(req.body.email);
+    DB.setUserValue(user.id , "verified" , true);
+  
+    res.json({
+        status : true,
+        data : "VERIFIED"
+    });
+};
