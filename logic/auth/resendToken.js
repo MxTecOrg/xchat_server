cef018e7256e7aebb2996fd0d8f0ee9e585d8f7e
MxTecInc <103785960+MxTecInc@users.noreply.github.com>
2022-04-18 12:49:43 -0400

    update resendToken.js

--- logic/auth/resendToken.js
+++ logic/auth/resendToken.js
@@ -1,2 +1,30 @@
 /**/
 const config = require("../../config.js");
+const DB = require(config.LOGIC + "/helpers/DB.js");
+const uid = require(config.LOGIC + "/helpers/uid.js");
+const sendToken = require("./sendToken.js");
+
+const resendToken = async (req , res) => {
+    if(!req.body || !req.body.email) return res.json({
+            status : false,
+            data : "WRONG_DATA"
+        });
+    
+    
+    const email = req.body.email;
+    const user = await DB.findUserByMail(email);
+    if(!user) return res.json({
+        status : false,
+        data : "USER_NOT_FOUND"
+    });
+    
+    await sendToken(email);
+    
+    res.json({
+        status: true,
+        data : "TOKEN_SENDED"
+    });
+    
+}
+
+module.exports = resendToken;
